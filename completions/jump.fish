27a870304b8a72095246b175dcaef1108f002206
Andreas Nordal <andreas_nordal_4@hotmail.com>
2016-04-21 23:12:25 +0200

    Complete options too

--- completions/jump.fish
+++ completions/jump.fish
@@ -1,5 +1,9 @@
 
-complete -c jump -n 'test (commandline) = "jump -"' -f -a '-h'
+complete -c jump -s h -l help -d 'Print usage'
+complete -c jump -s m -l mv -x -a '(command ls (jump --get-markpath))' -d 'Rename bookmark'
+complete -c jump -s r -l rm -x -a '(command ls (jump --get-markpath))' -d 'Delete bookmarks'
+complete -c jump -s s -l set -x -d 'Create bookmark'
+complete -c jump -s c -l setscm -x -d 'Create scm bookmark'
 
 function __fish_jump_suggest
 	set pre (commandline -co)
@@ -9,13 +13,19 @@ function __fish_jump_suggest
 		case 1
 			set pre
 		case 2
-			test $pre[2] = --
-			and set pre
-			or set pre $pre[2]
+			switch $pre[2]
+				case -- -p --print
+					set pre
+				case '*'
+					set pre $pre[2]
+			end
 		case '*'
-			test $pre[2] = --
-			and set pre $pre[3..-1]
-			or set pre $pre[2..-1]
+			switch $pre[2]
+				case -- -p --print
+					set pre $pre[3..-1]
+				case '*'
+					set pre $pre[2..-1]
+			end
 	end
 
 	set pre (jump -p $pre ^/dev/null)
@@ -40,4 +50,5 @@ function __fish_jump_suggest
 	end
 end
 
+complete -c jump -s p -l print -x -a '(__fish_jump_suggest)' -d 'Show destination'
 complete -c jump -n 'test (commandline) != "jump -"' -f -a '(__fish_jump_suggest)'
